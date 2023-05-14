4036a5393c332a9ad3ac8146da4c596555a840ee
Your Name <you@example.com>
2023-05-14 15:31:26 +0200

    menna enemy

--- player.gd
+++ player.gd
@@ -22,11 +22,27 @@ func _physics_process(delta):
 	look_at(get_global_mouse_position())
 	
 	if Input.is_action_just_pressed("fire"):
+		fire()
+	
+	
+func fire ():
 		var bullet_instance = bullet.instance()
 		bullet_instance.position = get_global_position()
 		bullet_instance.rotation_degrees=rotation_degrees
 		bullet_instance.apply_impulse(Vector2(),Vector2(bullet_speed,0).rotated(rotation))
 		get_tree().get_root().call_deferred("add_child",bullet_instance)
+		
+		
+func kill():
+	get_tree().reload_current_scene()
+
+
+func _on_Area2D_body_entered(body):
+	if "Enemy" in body.name :
+		kill()
 	
 	
 	
+	
+	
+
